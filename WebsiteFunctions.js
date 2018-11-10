796827a31afcc25e80beca4d2ec8bc68628821e0
EmmaLatham98 <43680538+EmmaLatham98@users.noreply.github.com>
2018-11-10 03:32:20 +0000

    Add files via upload

---
+++ WebsiteFunctions.js
@@ -0,0 +1,16 @@
+function getRow(e){
+	
+	//parse in status thats clicked
+	var target = e;
+	
+	//get value of status
+	
+	String status  = getStatus();
+	//change status 
+	
+	//get table row
+	
+	//get final columnn
+	.setAttribute("status", status);
+	
+}
