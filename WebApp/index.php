9ead35b0946111e4db94ab5f8cb6b857b99bc2cf
josheey3 <joshburtsal@gmail.com>
2018-11-10 07:15:36 +0000

    index
    Home button added

--- WebApp/index.php
+++ WebApp/index.php
@@ -16,7 +16,9 @@
 			} else { ?>
 				<a href="/login.php"><h5 align="right">Login</h5> </a> <?php
 			} ?>		
-			
+		<a href="/index.php">
+			<img src = "juliashouse_icon.jpg" class="home-btn" href="index.php">
+		</a>
 	</head>
 
 	<body>
