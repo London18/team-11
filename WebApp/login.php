d52d24ce9e32ba1f95fa1670de3d3d784cde1887
Jackclarke95 <33841002+Jackclarke95@users.noreply.github.com>
2018-11-10 04:22:28 +0000

    login
    align form 6

--- WebApp/login.php
+++ WebApp/login.php
@@ -12,9 +12,9 @@
 		<img src="julia'sHouseIcon2.jpg">
 		
 			<form method="post">
-				Staff ID: <input type="text" name="staffid" placeholder="Staff ID">
+				Staff ID: <input type="text" name="staffid" placeholder="Staff ID" class="mb-2 ml-3">
 				</br>
-				Password: <input type="password" name="password" placeholder="Password">
+				Password: <input type="password" name="password" placeholder="Password" class="mb-2">
 				</br>
 				<input type="submit">
 			</form>
