e5d8a4b846c9bfc7df6b36b16243af0f2e9ab902
EmmaLatham98 <43680538+EmmaLatham98@users.noreply.github.com>
2018-11-10 11:01:20 +0000

    Update WebsiteFunctions.js

--- WebApp/WebsiteFunctions.js
+++ WebApp/WebsiteFunctions.js
@@ -3,7 +3,7 @@ function doStuff(status){
 	alert("hello");
 	var bln = false;
 	var table = document.getElementById("tbl");
-	for (var i = 1, row; row = table.rows[i]; i++) {
+	for (var i = 1, row; row = table.rows[i]; i++){
 		var rowt = row.cells[5].innerHTML;
 		if((rowt === "-")&&(bln===false)){
 				row.cells[5].innerHTML=status;
