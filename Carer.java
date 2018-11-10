8960c0c3d302a324c9fa2cf1e0b4fd37a824300d
AncaMilea <aem1u17@soton.ac.uk>
2018-11-10 01:48:03 +0000

    #getMorefunnyBugs have buttons and change status

--- Carer.java
+++ Carer.java
@@ -1,15 +1,20 @@
+
 public class Carer {
     private int carerId;
     private String nameC;
     private String password;
     private String status;
+    ArrayList<Sit> sits;
+    Map buttonsPressed;
 
-    public Carer(int carerId, String nameC, String password, String status)
+    public Carer(int carerId, String nameC, String password, String status, Arraylist sits, buttonPressed)
     {
         this.carerId=carerId;
         this.nameC=nameC;
         this.password=password;
         this.status=status;
+        this.sits=sits;
+        this.buttonsPressed=buttonPressed;
     }
 
     public int getCarerId() {
@@ -30,5 +35,26 @@ public class Carer {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public ArrayList<Sit> getSits() {
+        return sits;
+    }
+
+    public Map getButtonPressed() {
+        return buttonsPressed;
+    }
+
+    public void setButtonPressed(String buttonPressed) {
+
+        for (Map.Entry<Integer, Integer> entry : this.buttonsPressed.entrySet())
+        {
+            if(entry.getValue==1)
+            {
+                entry.put(entry.getKey(),0);
+            }
+        }
+
+        this.buttonsPressed.put(buttonPressed,1);
     }
 }
