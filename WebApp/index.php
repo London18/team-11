1c3787392e0db0f2b6cb4a36fe231168c98153e6
Jackclarke95 <33841002+Jackclarke95@users.noreply.github.com>
2018-11-10 05:26:04 +0000

    index
    table width

--- WebApp/index.php
+++ WebApp/index.php
@@ -20,46 +20,46 @@
 
 	<body>
 		<h3>Julia's House - Home page</h3>
-		
-		<table class="table table-hover">
-			<thead>
-				<tr>
-					<th scope="col">Name</th>
-					<th scope="col">Start</th>
-					<th scope="col">End</th>
-					<th scope="col">Location</th>
-					<th scope="col">Notes</th>
-					<th scope="col">Status</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>get</td>
-					<td>9am</td>
-					<td>10am</td>
-					<td>53 Bank Lane, PO2 3OP</td>
-					<td>Going to the park</td>
-					<td>-</td>
-				</tr>
-				<tr>
-					<td>Jeek</td>
-					<td>11am</td>
-					<td>1pm</td>
-					<td>43 Lane Street, DY6 9ED</td>
-					<td>Give medication @ 12pm</td>
-					<td>-</td>
-				</tr>
-				<tr>
-					<td>Chris</td>
-					<td>2pm</td>
-					<td>6pm</td>
-					<td>295 Street Avenue, LE2 0PE</td>
-					<td>N/A</td>
-					<td>-</td>
-				</tr>
-			</tbody>
-		</table>
-		
+		<div class="col-sm 12">
+			<table class="table table-hover col-sm 10">
+				<thead>
+					<tr>
+						<th scope="col">Name</th>
+						<th scope="col">Start</th>
+						<th scope="col">End</th>
+						<th scope="col">Location</th>
+						<th scope="col">Notes</th>
+						<th scope="col">Status</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>get</td>
+						<td>9am</td>
+						<td>10am</td>
+						<td>53 Bank Lane, PO2 3OP</td>
+						<td>Going to the park</td>
+						<td>-</td>
+					</tr>
+					<tr>
+						<td>Jeek</td>
+						<td>11am</td>
+						<td>1pm</td>
+						<td>43 Lane Street, DY6 9ED</td>
+						<td>Give medication @ 12pm</td>
+						<td>-</td>
+					</tr>
+					<tr>
+						<td>Chris</td>
+						<td>2pm</td>
+						<td>6pm</td>
+						<td>295 Street Avenue, LE2 0PE</td>
+						<td>N/A</td>
+						<td>-</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 		
 		<form method="post">
 			<input type="submit" name="onWay" id="onWay" value="Going" class = "btn btn-primary"/>
