8f3f2ad71263ba875cb75da73b8c3775d438017f
Jackclarke95 <33841002+Jackclarke95@users.noreply.github.com>
2018-11-10 04:01:02 +0000

    Login page
    align centre

--- WebApp/login.php
+++ WebApp/login.php
@@ -7,12 +7,12 @@
 	</head>
 
 	<body>	
-		<div class="center">
+		<div align="center">
 		<img src="julia'sHouseIcon2.jpg">
 		
 		<table class="table table-hover">
 			<form method="post">
-				Staff ID: <input type="text" name="staffid      " placeholder="Staff ID"> <br>
+				Staff ID: <input type="text" name="staffid" placeholder="Staff ID"> <br>
 				Password: <input type="password" name="password" placeholder="Password"> <br>
 				<input type="submit">
 			</form>
