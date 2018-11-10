e605ca45e48b3cfa0734366fff4ec36c9945da8e
Jackclarke95 <33841002+Jackclarke95@users.noreply.github.com>
2018-11-10 04:32:32 +0000

    index css fixes
    version 1

--- WebApp/index.php
+++ WebApp/index.php
@@ -63,10 +63,10 @@
 		
 		
 		<form method="post">
-			<input type="submit" name="onWay" id="onWay" value="On my way"/>
-			<input type="submit" name="arrived" id="arrvied" value="Arrived" />
-			<input type="submit" name="finished" id="finished" value="Finished"/>
-			<input type="submit" name="submit" id="submit" value="Submit" style="float:right;"/>
+			<input type="submit" name="onWay" id="onWay" value="On my way" class = "btn btn-primary"/>
+			<input type="submit" name="arrived" id="arrvied" value="Arrived" class = "btn btn-primary"/>
+			<input type="submit" name="finished" id="finished" value="Finished" class = "btn btn-primary"/>
+			<input type="submit" name="submit" id="submit" value="Submit" style="float:right;" class = "btn btn-primary"/>
 		</form>
 	
 		<?php 
