1c5dfa57073508ce43be3cdfbb749c68c5bb0f64
Jackclarke95 <33841002+Jackclarke95@users.noreply.github.com>
2018-11-10 06:57:08 +0000

    index
    submit button position fix attempt 13

--- WebApp/index.php
+++ WebApp/index.php
@@ -66,7 +66,7 @@
 			<input type="submit" name="onWay" id="onWay" value="Going" class = "btn btn-primary"/>
 			<input type="submit" name="arrived" id="arrvied" value="Arrived" class = "btn btn-primary"/>
 			<input type="submit" name="finished" id="finished" value="Finished" class = "btn btn-primary"/>
-			<input type="submit" name="submit" id="submit" value="Submit" class = "btn btn-primary float-right"/>
+			<input type="submit" name="submit" id="submit" value="Submit" class = "btn btn-primary col-sm-offset-10"/>
 		</form>
 	
 		<?php 
