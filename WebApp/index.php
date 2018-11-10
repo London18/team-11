bcbc2e29f72fe68797adaf72f8c9216d7d4f4970
EmmaLatham98 <43680538+EmmaLatham98@users.noreply.github.com>
2018-11-10 10:48:17 +0000

    Update index.php

--- WebApp/index.php
+++ WebApp/index.php
@@ -66,9 +66,9 @@
 		</div>
 		
 		<form method="post" class="col-sm-12">
-			<input type="button" name="onWay" id="onWay" value="Going" class = "btn btn-primary" onclick="doStuff(value)"/>
-			<input type="button" name="arrived" id="arrvied" value="Arrived" class = "btn btn-primary" onclick="doStuff(value)"/>
-			<input type="button" name="finished" id="finished" value="Finished" class = "btn btn-primary" onclick="doStuff(value)"/>
+			<input type="submit" name="onWay" id="onWay" value="Going" class = "btn btn-primary" onclick="doStuff(value)"/>
+			<input type="submit" name="arrived" id="arrvied" value="Arrived" class = "btn btn-primary" onclick="doStuff(value)"/>
+			<input type="submit" name="finished" id="finished" value="Finished" class = "btn btn-primary" onclick="doStuff(value)"/>
 			
 		</form>
 	
